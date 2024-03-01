aa41b1d698f5acd286b60f0f047291117df353e1
arthagp <arthagusfi11@gmail.com>
2024-03-01 22:26:43 +0700

    added component lobbyRoom, and changed dashboard, navbar

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const LobbyRoom = () => {
+  // TODO: mengecek jika tidak ada orang yang masuk di code tersebut maka, break out room notif 'tidak ada orang disini', akan tetapi jika ada muncul avatar masing2 username 'AG'/'GP'..
+
+  return (
+    <div className="w-full h-16 min-h-screen bg-gray-200 flex justify-between items-center p-20">
+      <div className="w-1/2">
+        <div className="w-[600px] h-[500px] bg-black"></div>
+      </div>
+      <div className="flex flex-col justify-center items-center gap-10 w-1/2">
+        <div>
+          <h2 className="text-4xl font-semibold">Siap Bergabung?</h2>
+          {/* cek apakah ada room */}
+          <p>Tidak ada orang lain di sini.</p>
+        </div>
+        <div className="flex justify-center items-center gap-5">
+          <Button className="rounded-2xl">Gabung Sekarang</Button>
+          <Button className="rounded-2xl" variant={"secondary"}>
+            Share screen
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LobbyRoom;
--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,18 +1,21 @@
 import React from "react";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
   // logo, avatar (username), dan login/register , jika sudah login maka login/register disapper
   return (
-    <div className="w-full p-10 h-16 bg-gray-200 flex justify-between items-center">
+    <div className="w-full fixed inset-0 p-10 h-16 bg-gray-100 flex justify-between items-center">
       <div>
         <h1 className="text-neutral-600 text-3xl font-semibold">
           <span className="text-blue-400">Go</span>Meet
         </h1>
       </div>
       <div className="flex justify-center items-center gap-5">
+        <p className="text-neutral-600 font-light">Username</p>
         <Avatar>
+          {/* sementara avatar */}
+          <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>AGP</AvatarFallback>
         </Avatar>
         <span className="h-10 w-1 bg-black"></span>
